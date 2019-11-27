314ca24622d84923ebae2430af66b2ed057005a3
20181102931 <18647197975@163.com>
2019-11-27 11:41:27 +0800

    56

--- ask and answer/ViewController.swift
+++ ask and answer/ViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var index = 1;
+    @IBAction func input(_ sender: UITextField) {
+            switch index
+            {
+            case 1:
+              input.text = "中国的首都是北京吗？";
+            case 2:
+              input.text = "北京烤鸭是北京的好吃的吗？"
+            }
+        }
+    
+    }
+    
+    @IBAction func yes(_ sender: UIButton) {
+        index = index + 1;
+    }
+    
+    @IBAction func no(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
